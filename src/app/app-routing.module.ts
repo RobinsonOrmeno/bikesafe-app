7a22ce103c4f86e75c7a337c54291bcb89a169d1
Robinson <r.ormenomeza@gmail.com>
2020-11-23 17:51:57 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TermsConditionComponent } from './pages/terms-condition/terms-condition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+    },
+    {
+    path : 'login',
+    component : LoginComponent
+    },
+    {
+    path : 'about',
+    component : AboutComponent
+    },
+    {
+    path : 'terms-condition',
+    component : TermsConditionComponent
+    }
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RootNavComponent } from './root-nav/root-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { LoginComponent } from './pages/login/login.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TermsConditionComponent } from './pages/terms-condition/terms-condition.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootNavComponent,
+    LoginComponent,
+    ReviewComponent,
+    AboutComponent,
+    TermsConditionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
