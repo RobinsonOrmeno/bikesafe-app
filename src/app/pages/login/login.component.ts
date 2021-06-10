e8e08432ec2be078e0ae17a5898f26461b27b9ea
Robinson <r.ormenomeza@gmail.com>
2021-06-10 00:54:20 -0400

    avanzado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar, private router: Router) { 
+    this.form = this.fb.group({
+      usuario:['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  ingresar(){
+    console.log(this.form)
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+    if(usuario == 'robinson' && password == 'admin123'){
+        //redireciconar
+        this.fakeLoading()
+      }else{
+        this.error();
+        this.form.reset();
+      }
+
+  }
+  error(){
+    this._snackBar.open('Usuario o Contraseña ingresado son invalidos','',{
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+      
+  }
+
+  fakeLoading(){
+    this.loading = true;
+    setTimeout(()=>{
+      this.router.navigate(['perfil'])
+    }, 1500);
+  }
+
 
 }
